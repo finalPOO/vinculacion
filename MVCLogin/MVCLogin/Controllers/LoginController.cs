ae5d87c06630ec6fc0b027918292f6aa6c9369b7
CARVAJAL <roberto.carva97@gmail.com>
2018-07-01 23:37:18 -0500

    Version 1.0 Login all

---
+++ MVCLogin/MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLogin.Models;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Autherize(MVCLogin.Models.User userModel)
+        {
+            using (LoginDataBaseEntities db= new LoginDataBaseEntities())
+            {
+                var userDetails = db.User.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Usuario o contrasenia incorrecta";
+                    return View("Index", userModel);
+                }
+                else{
+                    Session["userID"] = userDetails.UserID;
+                    Session["userName"] = userDetails.UserName;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            
+        }
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
